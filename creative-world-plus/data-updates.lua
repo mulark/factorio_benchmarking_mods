b99a32f2805abc3aa49fbb4c74489a94fb80d782
keombre <keombre8@gmail.com>
2019-02-03 14:06:15 +0100

    add new world generator preset

---
+++ creative-world-plus/data-updates.lua
@@ -0,0 +1,36 @@
+data.raw["map-gen-presets"]["default"]["creative-world-plus"] =
+{
+	order = "w",
+	basic_settings =
+	{
+		autoplace_controls = {
+			["iron-ore"] = {
+				size = "none"
+			},
+			["copper-ore"] = {
+				size = "none"
+			},
+			["coal"] = {
+				size = "none"
+			},
+			["stone"] = {
+				size = "none",
+			},
+			["crude-oil"] = {
+				size = "none"
+			},
+			["uranium-ore"] = {
+				size = "none"
+            },
+            ["trees"] = {
+                size = "none"
+            }
+		},
+        water = "none",
+        cliff_settings = {
+            name = "none",
+            cliff_elevation_interval = 40,
+            cliff_elevation_0 = 1024
+        }
+	}
+}
