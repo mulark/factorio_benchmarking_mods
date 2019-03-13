cf1b9e08ba6e19eef4d4f8c5718554b5e171cfe7
mulark <mulark11@gmail.com>
2019-03-13 12:04:25 -0600

    old recipe mod

---
+++ old-recipe/data-final-fixes.lua
@@ -0,0 +1,5 @@
+data.raw["recipe"]["iron-plate"].energy_required = 3.5
+data.raw["assembling-machine"]["chemical-plant"].crafting_speed = 1.25
+data.raw["recipe"]["light-oil-cracking"].energy_required = 3
+data.raw["recipe"]["heavy-oil-cracking"].energy_required = 3
+data.raw["mining-drill"]["electric-mining-drill"].mining_time = 0.525
