47acfaeccfb268ff63fd2a685467aa54b45200d2
mulark <mulark11@gmail.com>
2019-10-11 20:13:44 -0600

    Creative world plus 0.0.9: 0.17 tiles

--- creative-world-plus/settings.lua
+++ creative-world-plus/settings.lua
@@ -1,9 +1,6 @@
 
--- all tiles (as of 0.16)
-local tiles = {"concrete", "deepwater", "deepwater-green", "dirt-1", "dirt-2", "dirt-3", "dirt-4", "dirt-5", "dirt-6", "dirt-7", "dry-dirt", "grass-1", "grass-2", "grass-3", "grass-4", "hazard-concrete-left", "hazard-concrete-right", "lab-dark-1", "lab-dark-2", "lab-white", "out-of-map", "red-desert-0", "red-desert-1", "red-desert-2", "red-desert-3", "refined-concrete", "refined-hazard-concrete-left", "refined-hazard-concrete-right", "sand-1", "sand-2", "sand-3", "stone-path", "tutorial-grid", "water", "water-green"}
-
--- mostly usefull tiles
--- local tiles = {"concrete", "refined-concrete", "lab-dark-1", "lab-dark-2", "lab-white", "stone-path", "tutorial-grid"}
+-- all tiles (as of 0.17.70)
+local tiles = {"concrete", "deepwater", "deepwater-green", "dirt-1", "dirt-2", "dirt-3", "dirt-4", "dirt-5", "dirt-6", "dirt-7", "dry-dirt", "grass-1", "grass-2", "grass-3", "grass-4", "hazard-concrete-left", "hazard-concrete-right", "lab-dark-1", "lab-dark-2", "lab-white", "landfill", "out-of-map", "red-desert-0", "red-desert-1", "red-desert-2", "red-desert-3", "refined-concrete", "refined-hazard-concrete-left", "refined-hazard-concrete-right", "sand-1", "sand-2", "sand-3", "stone-path", "tutorial-grid", "water", "water-green", "water-mud", "water-shallow"}
 
 data:extend({
     {
