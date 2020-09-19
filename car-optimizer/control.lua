9fd267056f02e8e422bca969853a5eaffda4318b
mulark <mulark11@gmail.com>
2020-09-18 19:43:37 -0600

    Release car-optimizer

--- car-optimizer/control.lua
+++ car-optimizer/control.lua
@@ -1,8 +1,8 @@
 require "util"
 
 
---[[In the future this will be rewritten for then general case. I want to only listen to the on_player_driving_changed_state event.]]
---[[TODO use count_entities_filtered() where applicable, should be more optimized]]
+--[[In the future this will be rewritten for then general case.
+    I want to only listen to the on_player_driving_changed_state event.]]
 
 local function round_to_closest_tile_with_offset(x)
     x = math.floor(x) + 0.5
@@ -16,6 +16,10 @@ function toggle_cars(entity)
     local x_coord = round_to_closest_tile_with_offset(entity.position.x)
     local y_coord = round_to_closest_tile_with_offset(entity.position.y)
     local surface = entity.surface
+    if (entity.speed ~= 0) then
+        entity.active = true
+        return
+    end
     if (entity.get_driver() or entity.get_passenger()) then
         entity.active = true
         return
@@ -27,32 +31,56 @@ function toggle_cars(entity)
     end
 end
 
-
+function find_cars_for_transport_belt(belt_ent)
+    local surface = belt_ent.surface
+    local possible_cars_found = surface.find_entities_filtered({type = "car", position = belt_ent.position})
+    if (possible_cars_found) then
+        for _, possible_car in pairs(possible_cars_found) do
+            if possible_car.type == "car" then
+                toggle_cars(possible_car)
+            end
+        end
+    end
+end
 
 script.on_event(defines.events.on_player_driving_changed_state, function(event)
     toggle_cars(event.entity)
 end)
 
-script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_entity}, function(event)
-    local action_entity = event.created_entity
-    toggle_cars(action_entity)
-    if (action_entity.type == "transport-belt" or action_entity.type == "splitter") then
-        local surface = event.created_entity.surface
-        --[[TODO: This only finds entities of name "car", while doing this for tanks too is required]]
-        local car_found = surface.find_entity("car", action_entity.position)
-        if (car_found) then
-           car_found.active = true
-        end
-    end
-end)
+script.on_event(defines.events.on_built_entity, function(event)
+    find_cars_for_transport_belt(event.created_entity)
+end,
+{{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"}}
+)
 
-script.on_event({defines.events.on_player_mined_entity, defines.events.on_robot_mined_entity}, function(event)
-    local action_entity = event.entity
-    if (action_entity.type == "transport-belt" or action_entity.type == "splitter") then
-        local surface = event.entity.surface
-        local car_found = surface.find_entity("car", action_entity.position)
-        if (car_found) then
-            toggle_cars(car_found)
-        end
-    end
-end)
+script.on_event(defines.events.on_built_entity, function(event)
+    toggle_cars(event.created_entity)
+end,
+{{filter="type", type = "car"}}
+)
+
+script.on_event(defines.events.on_robot_built_entity, function(event)
+    find_cars_for_transport_belt(event.created_entity)
+end,
+{{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"}}
+)
+
+script.on_event(defines.events.on_robot_built_entity, function(event)
+    toggle_cars(event.created_entity)
+end,
+{{filter="type", type = "car"}}
+)
+
+script.on_event(defines.events.on_player_mined_entity, function(event)
+    find_cars_for_transport_belt(event.entity)
+end,
+{{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"}}
+)
+
+script.on_event(defines.events.on_robot_mined_entity, function(event)
+    find_cars_for_transport_belt(event.entity)
+end,
+{{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"}}
+)
+
+--[[ TODO 0.18.10: inserters can be said to not chase items in their prototype!]]
