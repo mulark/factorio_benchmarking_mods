4ea8f95a7136a9973838663ec00ed259e9644ab3
mulark <mulark11@gmail.com>
2021-01-26 20:08:13 -0700

    0.0.6: Several fixes by Pi-C

--- car-optimizer/control.lua
+++ car-optimizer/control.lua
@@ -6,7 +6,7 @@ local function round_to_closest_tile_with_offset(x)
 end
 
 function toggle_cars(entity)
-    if not entity then
+    if not (entity and entity.valid) then
         return
     end
     if (entity.type ~= "car") then
@@ -33,53 +33,86 @@ end
 function find_cars_for_transport_belt(belt_ent)
     local surface = belt_ent.surface
     local possible_cars_found = surface.find_entities_filtered({type = "car", position = belt_ent.position})
-    if (possible_cars_found) then
-        for _, possible_car in pairs(possible_cars_found) do
-            if possible_car.type == "car" then
-                toggle_cars(possible_car)
-            end
+    for _, possible_car in pairs(possible_cars_found) do
+        toggle_cars(possible_car)
+        if possible_car.type == "car" then
         end
     end
 end
 
-script.on_event(defines.events.on_player_driving_changed_state, function(event)
-    toggle_cars(event.entity)
-end)
+function handle_event(event)
+    local entity = event.created_entity or	-- on_built events
+                 event.entity			-- on_player_driving_changed_state
+
+    if entity.type == "car" then
+    	toggle_cars(entity)
+    else
+        find_cars_for_transport_belt(entity)
+    end
+end
 
 script.on_event(defines.events.on_built_entity, function(event)
-    find_cars_for_transport_belt(event.created_entity)
-end,
-{{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"}}
-)
-
-script.on_event(defines.events.on_built_entity, function(event)
-    toggle_cars(event.created_entity)
-end,
-{{filter="type", type = "car"}}
-)
+    handle_event(event)
+end, {
+	{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"},
+	{filter="type", type = "car"}
+})
 
 script.on_event(defines.events.on_robot_built_entity, function(event)
-    find_cars_for_transport_belt(event.created_entity)
-end,
-{{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"}}
-)
-
-script.on_event(defines.events.on_robot_built_entity, function(event)
-    toggle_cars(event.created_entity)
-end,
-{{filter="type", type = "car"}}
-)
+    handle_event(event)
+end, {
+	{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"},
+	{filter="type", type = "car"}
+})
 
 script.on_event(defines.events.on_player_mined_entity, function(event)
-    find_cars_for_transport_belt(event.entity)
-end,
-{{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"}}
-)
+    handle_event(event)
+end, {
+	{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"}
+})
 
 script.on_event(defines.events.on_robot_mined_entity, function(event)
-    find_cars_for_transport_belt(event.entity)
-end,
-{{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"}}
-)
+    handle_event(event)
+end, {
+	{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"}
+})
+
+script.on_event(defines.events.on_entity_died, function(event)
+    handle_event(event)
+end, {
+	{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"}
+})
+
+--[[ No entity is valid here
+script.on_event(defines.events.on_entity_destroyed,
+function(event)
+    handle_event(event)
+end, {
+	{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"},
+	{filter="type", type = "car"}
+})]]
+
+script.on_event(defines.events.script_raised_built, function(event)
+    handle_event(event)
+end, {
+	{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"},
+	{filter="type", type = "car"}
+})
+
+script.on_event(defines.events.script_raised_destroy,
+function(event)
+    handle_event(event)
+end, {
+	{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"}
+})
+
+script.on_event(defines.events.script_raised_revive,
+function(event)
+    handle_event(event)
+end, {
+	{filter="type", type = "transport-belt"}, {filter="type", type = "splitter"},
+	{filter="type", type = "car"}
+})
+
 
 --[[ TODO 0.18.10: inserters can be said to not chase items in their prototype!]]
