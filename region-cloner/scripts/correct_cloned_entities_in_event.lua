1f5e2a3104fa464e43ff5869c6ffb49b0993946d
mulark <mulark11@gmail.com>
2024-12-07 10:11:06 -0700

    Fix cloning circuits not cloning to right connector in the case there were multiple

--- region-cloner/scripts/correct_cloned_entities_in_event.lua
+++ region-cloner/scripts/correct_cloned_entities_in_event.lua
@@ -7,13 +7,22 @@ function copy_circuit_network_reference_connections(original_entity, cloned_enti
         if connector ~= nil then
             for _,connection in pairs(connector.connections) do
                 local targetent = connection.target.owner
+                local target_wire_connector_id = connection.target.wire_connector_id
                 local offset_x = (original_entity.position.x - targetent.position.x)
                 local offset_y = (original_entity.position.y - targetent.position.y)
                 local targetnewent = cloned_entity.surface.find_entity({name=targetent.name, quality=targetent.quality}, {(cloned_entity.position.x - offset_x), (cloned_entity.position.y - offset_y)})
                 if targetnewent ~= nil then
                     local cloned_connector = cloned_entity.get_wire_connector(wire_connector_id, true)
-                    local cloned_connector_reference = targetnewent.get_wire_connector(wire_connector_id, true)
-                    cloned_connector.connect_to(cloned_connector_reference, false)
+                    local cloned_connector_reference = targetnewent.get_wire_connector(target_wire_connector_id, true)
+                    if cloned_connector_reference ~= nil then
+                        --game.print("Connecting " .. serpent.line(cloned_connector) .. " to " .. serpent.line(cloned_connector_reference))
+                        cloned_connector.connect_to(cloned_connector_reference, false)
+                    else
+                        -- Space platforms cloned to empty space means stuff dies leading to these being invalid before we set up the connection
+                        -- However, I don't know why sometimes the platform dies completely, and sometimes it doesn't
+                        -- it's supposed to be that if a platform is split in two the part without the hub dies
+                        game.print("ERROR: did not get a valid connector reference from " .. serpent.line(cloned_connector) .. " to " .. serpent.line(targetnewent))
+                    end
                 end
                 targetent = nil
                 offset_x = nil
